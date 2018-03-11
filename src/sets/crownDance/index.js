43e91af91d18b320a92918f83fbc84c9ada7784c
Nicola Bertelloni <nicola.bertelloni@gmail.com>
2018-03-11 11:22:31 +0100

    minor fixes

--- src/sets/crownDance/index.js
+++ src/sets/crownDance/index.js
@@ -5,15 +5,15 @@ let revolution = 0;
 
 export default function(p, vals, mic) {
   const {ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT} = vals;
-
-  const soundVal = parseInt((mic.getLevel() * 200), 10);
-
+  // p.colorMode(p.HSB)
+  const soundVal = mic.getLevel() * 100000;
+  p.smooth(.9)
   const color = {
     red: soundVal / 5,
     green: 125,
     blue: 125,
-    alphaBg: FIVE / 200,
-    alphaFg: SIX / 200,
+    alphaBg: FIVE,
+    alphaFg: SIX,
   };
 
   const bgColor = `rgba(
@@ -23,27 +23,29 @@ export default function(p, vals, mic) {
     ${color.alphaBg})`;
 
   const fillColor = `rgba(
-    ${color.red},
-    ${color.green}, 
-    ${color.blue}, 
-    ${color.alphaFg})`;
-
-  const scaleVal = Math.max(soundVal / (FOUR / 10), soundVal / 100)
+      ${color.red},
+      ${color.green}, 
+      ${color.blue}, 
+      ${color.alphaFg})`;
+  
+  const scaleVal = soundVal / (FOUR * 10)
+  console.log(scaleVal)
   p.translate(p.width / 2, p.height / 2);
-  p.background(bgColor);
+  p.background(color.red, color.green, color.blue, color.alphaBg);
   p.angleMode(p.DEGREES);
   p.rotate(orbit);
   orbit += EIGHT / 100;
   revolution += SEVEN / 100;
   p.stroke('white');
   p.fill(fillColor);
-  p.scale(scaleVal / 2)
+  p.scale(scaleVal);
   const els = Math.max(parseInt(THREE / 12.5, 10), 1);
   // const els = 3;
   const innerAngle = 360 / els;
   const side = ONE * 4;
   const radius = Math.min(TWO * 10, p.width / 2, p.height / 2);
-
+  
+  
   p.rotate(orbit);
   let i = 0;
   do {
